a14952ba2f17f27c375d3a1546669e457b4aa3f4
XJDHDR <XJDHDR@users.noreply.github.com>
2022-06-21 16:12:14 +1200

    - Scripting   - Added logic for checking the time since last update check against the check delay specified in settings.   - Also fleshed out the message displayed if the version numbers don't match.

--- 5 - Non Prefabs/UBLAMF.cs
+++ 5 - Non Prefabs/UBLAMF.cs
@@ -5,6 +5,9 @@
 
 using System;
 using System.Collections;
+using System.Globalization;
+using System.Linq;
+using System.Text;
 using UnityEngine;
 using DaggerfallWorkshop.Game;
 using DaggerfallWorkshop.Game.Serialization;
@@ -13,6 +16,8 @@ using DaggerfallWorkshop.Game.Utility;
 using DaggerfallWorkshop.Game.Utility.ModSupport;
 using DaggerfallWorkshop.Game.Utility.ModSupport.ModSettings;
 using UnityEngine.Networking;
+using UnityEngine.Windows;
+using UnityEngine.WSA;
 
 // ReSharper disable once CheckNamespace
 namespace UBLAMFMod
@@ -52,37 +57,76 @@ namespace UBLAMFMod
 
         private static IEnumerator GetLatestVersionNumberFromRepo()
         {
-            UnityWebRequest webRequest = UnityWebRequest.Get("https://raw.githubusercontent.com/XJDHDR/DFU_UBLaMF/master/Helpers%20and%20Cheat%20Sheets/Faction%20codes.url");
+            // Retrieve the last update time (UTC) from the PersistentDataDirectory.
+            DateTime lastUpdateTime;
+            if (Directory.Exists(mod.PersistentDataDirectory))
+            {
+                if (File.Exists($"{mod.PersistentDataDirectory}/LastUpdateTime.txt"))
+                {
+                    byte[] fileContents = File.ReadAllBytes($"{mod.PersistentDataDirectory}/LastUpdateTime.txt");
+                    lastUpdateTime = DateTime.FromBinary(BitConverter.ToInt64(fileContents, 0));
+                }
+                else
+                {
+                    lastUpdateTime = DateTime.MinValue;
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(mod.PersistentDataDirectory);
+                lastUpdateTime = DateTime.MinValue;
+            }
+            
+            // Compare the retrieved last update time against the current time and date.
+            // Abort if it's still too soon for another check.
+            TimeSpan timeSinceLastCheck = DateTime.UtcNow - lastUpdateTime;
+            if (timeSinceLastCheck.Days < updateInterval)
+                yield break;
+            
+            // Download the text file containing the version number for the latest release.
+            UnityWebRequest webRequest = UnityWebRequest.Get("https://raw.githubusercontent.com/XJDHDR/DFU_UBLaMF/master/Latest_version.txt");
             yield return webRequest.SendWebRequest();
 
             // If UnityWebRequest failed to download the latest version file, just abort the update checker.
             if (webRequest.error != null || webRequest.isNetworkError || webRequest.isHttpError)
                 yield break;
-            
-            int downloadedVersionNumberLength = webRequest.downloadHandler.text.Length;
-            char[] downloadedVersionNumberStripped = new char[downloadedVersionNumberLength];
-            int stringIndexCounter = 0;
-            int strippedStringIndexCounter = 0;
-            while (stringIndexCounter < downloadedVersionNumberLength)
+
+            // Strip any NULL, newline, and whitespace characters from the downloaded data.
+            int downloadedVersionNumberLength = webRequest.downloadHandler.data.Length;
+            byte[] downloadedVersionNumberStripped = new byte[downloadedVersionNumberLength];
+            int fullByteArrayIndexCounter = 0;
+            int strippedByteArrayIndexCounter = 0;
+            while (fullByteArrayIndexCounter < downloadedVersionNumberLength)
             {
-                char currentChar = webRequest.downloadHandler.text[stringIndexCounter];
-                switch(currentChar)
+                switch(webRequest.downloadHandler.data[fullByteArrayIndexCounter])
                 {
-                    case ' ':
-                    case '\n':
-                    case '\r':
-                    case '\t':
-                        // Ignore then
+                    case 0x00:  // NULL
+                    case 0x09:  // TAB
+                    case 0x0A:  // LINE FEED
+                    case 0x0D:  // CARRIAGE RETURN
+                    case 0x20:  // SPACE
+                        // Skip to the next byte
                         break;
 
                     default:
-                        downloadedVersionNumberStripped[strippedStringIndexCounter++] = currentChar;
+                        downloadedVersionNumberStripped[strippedByteArrayIndexCounter++] = webRequest.downloadHandler.data[fullByteArrayIndexCounter];
                         break;
                 }
-                ++stringIndexCounter;
+                ++fullByteArrayIndexCounter;
             }
-            downloadedVersionNumber = new string(downloadedVersionNumberStripped, 0, strippedStringIndexCounter);
+            
+            // Is the version number for this installed copy of the mod equal to the processed version number?
+            //                         2      0     2     1     .     0     9     .     0     0
+            byte[] thisModVersion = { 0x32, 0x30, 0x32, 0x31, 0x2E, 0x30, 0x39, 0x2E, 0x30, 0x30 };
+            if (downloadedVersionNumberStripped.SequenceEqual(thisModVersion))
+            {
+                Debug.LogWarning("Versions equal");
+                File.WriteAllBytes($"{mod.PersistentDataDirectory}/LastUpdateTime.txt", BitConverter.GetBytes(DateTime.UtcNow.ToBinary()));
+                yield break;
+            }
 
+            // Populate the version number field for the messagebox, and register for the New Game and Load Save events.
+            downloadedVersionNumber = Encoding.ASCII.GetString(downloadedVersionNumberStripped);
             SaveLoadManager.OnLoad += SaveGameLoaded;
             StartGameBehaviour.OnNewGame += NewGameStarted;
         }
@@ -96,11 +140,27 @@ namespace UBLAMFMod
         {
             SaveLoadManager.OnLoad -= SaveGameLoaded;
             StartGameBehaviour.OnNewGame -= NewGameStarted;
+            File.WriteAllBytes($"{mod.PersistentDataDirectory}/LastUpdateTime.txt", BitConverter.GetBytes(DateTime.UtcNow.ToBinary()));
+            
             DaggerfallMessageBox messageBox = new DaggerfallMessageBox(DaggerfallUI.UIManager, DaggerfallUI.UIManager.TopWindow, true)
             {
                 ClickAnywhereToClose = true
             };
-            messageBox.SetText(downloadedVersionNumber);
+            DateTime nextUpdateCheck = DateTime.Now + new TimeSpan(updateInterval, 0, 0, 0);
+            string[] messageLines =
+            {
+                "Message from Unofficial Block, Locations and Model Fixes mod:",
+                "",
+                $"You are using an outdated version. The latest version available is {downloadedVersionNumber}, but you are using version 2021.09.00",
+                "It is recommended that you update to the latest version, which can be downloaded from Nexus Mods:",
+                "https://www.nexusmods.com/daggerfallunity/mods/100",
+                "",
+                "Unless you disable update checking or change the waiting period in the mod's settings, ",
+                $"the next update check will happen after {nextUpdateCheck.ToString(CultureInfo.CurrentCulture)}.",
+                "",
+                "Press any key to close this message."
+            };
+            messageBox.SetText(messageLines);
             messageBox.Show();
         }
     }
