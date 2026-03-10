e94f7cd99a5dc2bdd1e37afb9d31e2060121a035
XJDHDR <XJDHDR@users.noreply.github.com>
2026-03-11 12:42:28 +1300

    Reoragnised repo
    - Moved files around to better organise them.
    - In particular, files used by the editor to build the mod have all been moved into a single properly organised folder.

---
+++ Editor_files/Scripts/UBLAMF.cs
@@ -0,0 +1,87 @@
+﻿// ReSharper disable once CommentTypo
+// This file is or was originally a part of the Unofficial Block, Location and Model Fixes mod for Daggerfall Unity by XJDHDR,
+// which can be found here: https://github.com/XJDHDR/DFU_UBLaMF
+//
+// The license for it may be found here:
+// https://github.com/XJDHDR/DFU_UBLaMF/blob/master/Editor_files/Scripts/License-MIT.txt
+//
+
+using System;
+using System.Collections;
+using System.Text;
+using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Game.Utility.ModSupport;
+using DaggerfallWorkshop.Game.Utility.ModSupport.ModSettings;
+using UnityEngine;
+
+namespace UBLAMFMod
+{
+    public class Ublamf : MonoBehaviour
+    {
+	    // ==== Properties ====
+	    internal static bool _DontCheckForUpdatesSetting { get; private set; }
+	    internal static int _UpdateIntervalSetting { get; private set; }
+
+	    internal static Mod _UblamfModInstance { get; private set; }
+	    internal static ModSettings _UblamfModSettings { get; private set; }
+	    internal static StringBuilder _ModStringBuilder { get; } = new StringBuilder();
+
+
+        // ==== DFU and Unity initialisation methods ====
+        // ReSharper disable once UnusedMember.Global
+        [Invoke(StateManager.StateTypes.Start, 0)]
+        public static void Init(InitParams InitParams)
+        {
+	        try
+	        {
+	            _UblamfModInstance = InitParams.Mod;
+
+	            _UblamfModSettings = _UblamfModInstance.GetSettings();
+	            _DontCheckForUpdatesSetting = _UblamfModSettings.GetValue<bool>("UpdateOptions", "DisableUpdateCheck");
+	            _UpdateIntervalSetting = _UblamfModSettings.GetValue<int>("UpdateOptions", "UpdateInterval");
+
+	            GameObject go = new GameObject("UBLaMF manager");
+	            go.AddComponent<Ublamf>();
+
+	            _UblamfModInstance.IsReady = true;
+	        }
+	        catch (Exception e)
+	        {
+		        _ModStringBuilder.Clear();
+		        _ModStringBuilder.Append($"UBLaMF: An exception occurred in the {nameof(Ublamf)} - ");
+		        _ModStringBuilder.AppendLine($"{nameof(Init)} method.");
+		        _ModStringBuilder.AppendLine("Arguments:");
+		        _ModStringBuilder.AppendLine($"  Arg1: {nameof(InitParams)}:{nameof(InitParams)} - {InitParams.ToString()}.");
+		        _ModStringBuilder.AppendLine($"{e}");
+		        Debug.LogError(_ModStringBuilder.ToString());
+		        _ModStringBuilder.Clear();
+	        }
+        }
+
+        private void Start()
+        {
+	        try
+	        {
+				UblamfUpdateCheck._GetDfuVersionNumberAndStartCheckerRoutine(this);
+	        }
+	        catch (Exception e)
+	        {
+		        _ModStringBuilder.Clear();
+		        _ModStringBuilder.Append($"UBLaMF: An exception occurred in the {nameof(Ublamf)} - ");
+		        _ModStringBuilder.AppendLine($"{nameof(Start)} method.");
+		        _ModStringBuilder.AppendLine($"{e}");
+		        Debug.LogError(_ModStringBuilder.ToString(), this);
+		        _ModStringBuilder.Clear();
+	        }
+        }
+
+
+        // ==== Internal methods ====
+        /// <summary>
+        /// This helper method is used to start Coroutines from classes and structs that don't extend MonoBehaviour
+        /// </summary>
+        /// <param name="Routine">The method that will be run inside the Coroutine.</param>
+        internal void _StartCoroutineHelper(IEnumerator Routine) =>
+	        StartCoroutine(Routine);
+    }
+}
---
+++ Helpers_and_Cheat_Sheets/Convert Model Fixes models to HTML.ahk
@@ -0,0 +1,52 @@
+﻿
+
+
+Loop, Read, %A_ScriptDir%\input.txt
+{
+	If ((RegExMatch(A_LoopReadLine, "^[a-zA-Z]+$") == 0) && (RegExMatch(A_LoopReadLine, "^[0-9]+$") != 0))
+	{
+		bBlankLineFound := False
+		iModelNumber := A_LoopReadLine
+		sModelNumberFiveDigits := SubStr("00000" . A_LoopReadLine, -4)
+	}
+	Else If (InStr(A_LoopReadLine, "Description: ") != 0)
+	{
+		arDescriptionParts := StrSplit(A_LoopReadLine, ":", " `t", 2)
+		sDescription := arDescriptionParts[2]
+		arDescriptionParts := ""
+	}
+	Else If (InStr(A_LoopReadLine, "e.g.") != 0)
+	{
+		arExampleLocationParts := StrSplit(A_LoopReadLine, ".", " `t", 3)
+		sExampleLocation := StrReplace(arExampleLocationParts[3], ")")
+		arExampleLocationParts := ""
+	}
+	Else If (InStr(A_LoopReadLine, "-") != 0)
+	{
+		arBugsFixedParts := StrSplit(A_LoopReadLine, "-", " `t", 2)
+		sBugsFixed .= "								<li>" . arBugsFixedParts[2] . "</li>`r`n"
+		arBugsFixedParts := ""
+	}
+	Else If (InStr(A_LoopReadLine, "") != 0)
+	{
+		If (bBlankLineFound != True)
+		{
+			bBlankLineFound := True
+			sOutputContents .= "					<tr>`r`n"
+			sOutputContents .= "						<td class=""data-table-cell-left""><a href=""https://github.com/XJDHDR/DFU_UBLaMF/blob/master/3%20-%20Exported%20OBJs%20%26%20FBXs/" . sModelNumberFiveDigits . "/" . iModelNumber . ".obj"" rel=""external"" onclick=""target='_blank'"">" . iModelNumber . "</a> <sup><span title=""" . sDescription . """>desc</span></sup></td>`r`n"
+			sOutputContents .= "						<td class=""data-table-cell-mid""><a href=""https://github.com/XJDHDR/DFU_UBLaMF/tree/master/5%20-%20Non%20Prefabs/"" rel=""external"" onclick=""target='_blank'"">Block</a> <sup><span title=""" . sExampleLocation . """>e.g. loc.</span></sup></td>`r`n"
+			sOutputContents .= "						<td class=""data-table-cell-mid"">2021.01.04</td>`r`n"
+			sOutputContents .= "						<td class=""data-table-cell-right"" style=""text-align: left;"">`r`n"
+			sOutputContents .= "							<ul class=""minimal-padding"">`r`n"
+			sOutputContents .= sBugsFixed
+			sOutputContents .= "							</ul>`r`n"
+			sOutputContents .= "						</td>`r`n"
+			sOutputContents .= "					</tr>`r`n"
+			sBugsFixed := ""
+		}
+	}
+}
+
+FileAppend, %sOutputContents%, Output.txt, UTF-8
+
+ExitApp
